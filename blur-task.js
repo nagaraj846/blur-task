0624e623b84d92107d852b255a3531e15ebfa875
nallurishivashankar <nallurisivashankar@gmail.com>
2019-12-03 09:05:22 +0530

    hi

--- blur-task.js
+++ blur-task.js
@@ -1,58 +1,40 @@
-// var name = "nagaraju";
 
 
-// // console.log(name.length)
-// // document.write(name.charAt(2))
-
-// console.log(name.indexOf("n"))
-
-// if (name.indexOf("n") <-1) {
-//     console.log(true);
-// }
-// else{
-//     console.log(false);
-// }
-
-
-// var age="5.995252";
-// var convertednumber = parseFloat(age);
-
-// document.write(convertednumber.toFixed(3))
-
-
-
-// var arr=[1,2,3,4,10,5,15,3]
-
-// console.log(arr[arr.length-1])
-
-// console.log(arr[Math.floor(arr.length/2)])
-
-// console.log(arr[(arr.length/2)-1], arr[arr.length/2])
-
-// console.log(arr.pop())
-// console.log(arr)
+var apidata;
+fetch('https://reqres.in/api/products')
+    .then(response => response.json())
+    .then(json => {
+        apidata = json.data;
+        console.log(apidata)
+        var elements = apidata.map((value) => {
+            return (
+                `
+            
+                      
+              
+        
+                <h2>Card Image</h2>
+                <div class="card" style="width:400px">
+                  <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+                  <div class="card-body">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    <a href="#" class="btn btn-primary">See Profile</a>
+                  </div>
+                  </div>
+              
+              </div>
+           
+           `
+            )
 
 
-// console.log(arr.shift())
-// console.log(arr)
 
-// console.log(arr.push(20))
-// console.log(arr)
-
-// console.log(arr.unshift(222))
-// console.log(arr)
+        });
 
 
-// console.log(arr.slice(0,4))
-// console.log(arr)
-
-
-// console.log(arr.splice(0,2))
-// console.log(arr)
-
-
-// console.log(arr.splice(2,0,20))
-// console.log(arr)
+        document.getElementById('gun').innerHTML = elements;
+    })
 
 
 
@@ -64,61 +46,4 @@
 
 
 
-// var firsttextwithn=[]
-// for (i = 0; i < arr.length; i++) {
-//     if (arr[i][0] === "j") {
-//         firsttextwithn.push(arr[i])
-//     }
 
-// }
-
-
-// console.log(firsttextwithn)
-// var arr = ["nagaraju", "nani", "anil", "jayanth"]
-// function text(data, searchString) {
-//     var firsttextwithn = [];
-//     for (i = 0; i < data.length; i++) {
-//         if (data[i][0] === "n") {
-//             firsttextwithn.push(data[i])
-//         }
-
-//     }
-//     console.log(firsttextwithn)
-// }
-
-// text(arr)
-
-
-var nagaraju = {
-    name: "nagaraju",
-    age: 20,
-    dob: "10/05/1996"
-
-}
-
-var jayanth = {
-   na:"nag",
-}
-var nag=Object.assign(nagaraju,jayanth);
-console.log(nag)
-// for (let name in nagaraju) {
-//     // console.log(name)
-//     console.log(nagaraju[name])
-// }
-
-
-// var name=[
-//     {
-//         name:"nag",
-//         age:20
-
-//     },
-
-//         {
-//             name:"nag",
-//             age:20
-//         }
-
-
-// ]
-// console.log(name.length)
